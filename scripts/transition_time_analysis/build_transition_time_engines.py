8080320ceb69a665570e84d9bf4161252b3b4b18
Eymen Ünay <eymenunay@outlook.com>
2023-11-25 12:17:35 -0700

    Revert "Make engine build 30% faster with parallel build"
    This reverts commit 84888b903c1330101e753a525cb9091354b8b702.

--- scripts/transition_time_analysis/build_transition_time_engines.py
+++ scripts/transition_time_analysis/build_transition_time_engines.py
@@ -12,7 +12,6 @@ from natsort import natsorted
 import time
 from pathlib import Path
 import glob
-import concurrent.futures
 
 import argparse
 
@@ -93,56 +92,40 @@ def save_engine(serialized_engine, save_file):
     with open(save_file, "wb") as f:
         f.write(serialized_engine)
 
-
-
 script_dir = Path(__file__).resolve().parent
 root_path = script_dir.parent.parent
 prototxt = root_path/"prototxt_input_files/googlenet.prototxt"
 output_dir_path = root_path/"build/googlenet_transition_plans/"
 Path(output_dir_path).mkdir(parents=True, exist_ok=True)
 
-def build_and_save_engine(transition, model_file, deploy_file, runs_on_gpu, batch, output_name):
-    serialized_engine = build_engine_caffe(model_file, deploy_file, transition, runs_on_gpu, batch)
-    if serialized_engine is not None:
-        save_engine(serialized_engine.serialize(), output_name)
-
-# transition = -1
-
-def main(max_workers=4):  # Default to 4, adjust as needed
-    script_dir = Path(__file__).resolve().parent
-    root_path = script_dir.parent.parent
-    prototxt = root_path / "prototxt_input_files" / "googlenet.prototxt"
-
-    transition = {-1, 10, 24, 38, 53, 67, 81, 95, 109, 124, 139} # transitin cost for googlenet.  -1 is baseline
-    batch = 1
-    runs_on_gpu = True
-
-    try:
-        with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
-            # GPU transitions
-            futures_gpu = [executor.submit(build_and_save_engine, transition, None, str(prototxt), runs_on_gpu, batch, str(output_dir_path / f"googlenet_gpu_transition_at_{transition}.plan")) for transition in transitions]
-            
-            # DLA transitions
-            futures_dla = [executor.submit(build_and_save_engine, transition, None, str(prototxt), False, batch, str(output_dir_path / f"googlenet_dla_transition_at_{transition}.plan")) for transition in transitions]
-
-            # Wait for all futures to complete
-            for future in concurrent.futures.as_completed(futures_gpu + futures_dla):
-                future.result()  # This will re-raise any exception raised during execution
-
-    except Exception as e:
-        print(f"Parallel execution failed: {e}. Falling back to sequential execution.")
-        for transition in transitions:
-            # Sequentially build and save engines for GPU transitions
-            build_and_save_engine(transition, None, str(prototxt), runs_on_gpu, batch, str(output_dir_path / f"googlenet_gpu_transition_at_{transition}.plan"), args.verbose)
-            # Sequentially build and save engines for DLA transitions
-            build_and_save_engine(transition, None, str(prototxt), False, batch, str(output_dir_path / f"googlenet_dla_transition_at_{transition}.plan"), args.verbose)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Engine Building Script")
-    parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
-    parser.add_argument('-j', type=int, help='Number of workers')
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     if(not args.verbose):
         print("For more information run the script with --verbose option.")
-    main(max_workers=args.j) 
+
+    count = 0
+    batch = 1
+    transition = -1
+    for transition in {0, 10, 24, 38, 53, 67, 81, 95, 109, 124, 141}:
+        serialized_engine = build_engine_caffe(None, str(prototxt), transition, True, batch, args.verbose)
+        if serialized_engine is not None:
+            # Optimize path then create string
+            output_file = str( output_dir_path / f"googlenet_gpu_transition_at_{transition}.plan")
+            save_engine(
+                serialized_engine.serialize(),
+                output_file,
+            )
+            print("Saved engine " + output_file)
+    for transition in {0, 10, 24, 38, 53, 67, 81, 95, 109, 124, 141}:
+        serialized_engine = build_engine_caffe(None, str(prototxt), transition, True, batch, args.verbose)
+        if serialized_engine is not None:
+            output_file = str(output_dir_path / f"googlenet_dla_transition_at_{transition}.plan")
+            save_engine(
+                serialized_engine.serialize(),
+                output_file,
+            )
+            print("Saved engine " + output_file)
